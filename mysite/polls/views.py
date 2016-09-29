57934abeaea0054a9a85fa93fe52d53cca5fc932
Yuya ONUMA <yohnuma@gion-xy.net>
2016-09-29 20:55:32 +0900

    'Raising a 404 error' - get_object_or_404()
    - 第 1 引数に Django モデル，その後に任意のキーワード引数を渡すとそのキーワード引数を Django モデルの `.get()` に渡した結果を返す
    - `.get()` の結果何も返らないなら `Http404` 例外を送出

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
-from django.http import (
-    HttpResponse,
-    Http404,
+from django.http import HttpResponse
+from django.shortcuts import (
+    render,
+    get_object_or_404,
 )
-from django.shortcuts import render
 
 from .models import Question
 
@@ -16,10 +16,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
