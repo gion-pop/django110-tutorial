576b874330f582cfd9d611de79e4aa1decb2f0a0
Yuya ONUMA <yohnuma@gion-xy.net>
2016-09-29 22:35:30 +0900

    'Write a simple form' - implement vote view
    - `request.POST` で POSTデータを取得できる
    - POST には `HttpResponseRedirect` を返す（二重POST防止）
    - `reverse()` で URL を組み立てることでビューと URL 構成を分離
    - この実装だと競合が起きて正しく投票数が処理されない可能性がある

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import (
     render,
     get_object_or_404,
 )
+from django.urls import reverse
+from django.db.models import F
 
-from .models import Question
+from .models import (
+    Question,
+    Choice,
+)
 
 
 def index(request):
@@ -26,4 +31,15 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question {}.".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id, )))
