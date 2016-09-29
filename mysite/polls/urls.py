93c296fc90014638c35c3682418247f44bcc808c
Yuya ONUMA <yohnuma@gion-xy.net>
2016-09-29 21:22:35 +0900

    ’Namespacing URL names’
    - `name=detail` な `url` が複数の App にあったらどうする？
    - `app_name` で URL の名前空間を作る
    - `url` テンプレートタグに名前空間を含んだビュー名を指定すればいい

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
