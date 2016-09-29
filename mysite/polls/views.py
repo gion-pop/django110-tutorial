9d076c258a93ad8fa5fd643805a54f00da5a16b1
Yuya ONUMA <yohnuma@gion-xy.net>
2016-09-29 22:47:35 +0900

    'Write a simple form' - Note
    - 競合を防ぐために `django.db.models.F` を使う https://docs.djangoproject.com/en/1.10/ref/models/expressions/#f-expressions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -40,6 +40,6 @@ def vote(request, question_id):
             'error_message': "You didn't select a a choice.",
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question_id, )))
