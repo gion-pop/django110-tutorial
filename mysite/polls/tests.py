311b5b175a76c198d509ba8cb127cdbb0abbe6ab
Yuya ONUMA <yohnuma@gion-xy.net>
2016-10-05 01:44:11 +0900

    'Writing our first test' - 'More comprehensive tests'

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -9,9 +9,27 @@ from .models import Question
 class QuestionMethodTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
-        was_published_recently() should return `False` for questions
-        whose pub_date is in the future.
+        `was_published_recently()` should return `False` for questions
+        whose `pub_date` is in the future.
         """
         future = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=future)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        `was_published_recently()` should return `False` for questions
+        whose `pub_date` is older than 1 day.
+        """
+        past = timezone.now() - datetime.timedelta(days=30)
+        past_question = Question(pub_date=past)
+        self.assertIs(past_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        `was_published_recently()` should return `True` for questions
+        whose `pub_date` is within the last day.
+        """
+        recent = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=recent)
+        self.assertIs(recent_question.was_published_recently(), True)
