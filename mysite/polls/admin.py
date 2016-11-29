4c3a693b6d7b133ded4d41648a2beeae193c0e36
Yuya ONUMA <gion_XY@live.jp>
2016-11-29 14:02:03 +0900

    'Adding related objects' (2)
    - `Choice` は `Question` あってのものだから， `Question` の作成画面で一緒に作りたい

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -6,11 +6,16 @@ from .models import (
 )
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
